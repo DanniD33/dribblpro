f68c193a3cb00bb311cad8102de8c8742b54a638
DanniD33 <danni.denmark7@gmail.com>
2024-05-27 16:04:00 -0400

    changed port num

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 var path = require('path');
 const app = express();
 // const port = 5000;
-const port = 80;
+const port = 8080;
 
 
 app.get('/', (req, res) => {
