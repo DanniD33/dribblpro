ba5fbcd975a4b1293bbbf910d197faa279b6f85d
DanniD33 <danni.denmark7@gmail.com>
2024-05-27 18:38:23 -0400

    changed port number back temporqrily an i dont see thee connetion error i usually see in the logs

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 var path = require('path');
 const app = express();
-const port = 3000;
-// const port = 8080;
+// const port = 3000;
+const port = 8080;
 require('dotenv').config();
 
 
