2e2517135d152799e12d4e7d48a780b79975ae18
DanniD33 <danni.denmark7@gmail.com>
2024-05-27 17:32:55 -0400

    includd changes from removing package json lock. cleared cache.

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+var path = require('path');
+const app = express();
+// const port = 5000;
+const port = 8080;
+require('dotenv').config();
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on outer endpoints port ${port}`);
+    });
